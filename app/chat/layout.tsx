8445bef882281296ea0b4158bcf4d170b0ccdd8f
Shanuti Tiwari <shanutiwari.work@gmail.com>
2025-07-16 19:31:51 +0530

    add: wrap RootChatLayout with ProtectedLayout for auth guard

--- app/chat/layout.tsx
+++ app/chat/layout.tsx
@@ -2,20 +2,22 @@ import React from "react";
 import { Header } from "@/components/header";
 import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { ProtectedLayout } from "@/components/layout/ProtectedLayout";
 
 const RootChatLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="min-h-screen bg-background relative flex">
-      <SidebarProvider>
-        <AppSidebar />
-        <SidebarInset>
-          <Header />
-          <div className="relative h-[calc(100vh_-64px)] overflow-hidden">
-            {children}
-          </div>
-        </SidebarInset>
-      </SidebarProvider>
-    </div>
+   
+      <div className="min-h-screen bg-background relative flex">
+        <SidebarProvider>
+          <AppSidebar />
+          <SidebarInset>
+            <Header />
+            <div className="relative h-[calc(100vh_-64px)] overflow-hidden">
+              {children}
+            </div>
+          </SidebarInset>
+        </SidebarProvider>
+      </div>
   );
 };
 
