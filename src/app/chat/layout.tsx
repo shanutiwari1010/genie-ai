9f5d1087947257d40fc54c6da0b1c6ab7a8bbcf3
Shanuti Tiwari <shanutiwari.work@gmail.com>
2025-09-01 00:17:35 +0530

    refactor: remove ProtectedLayout wrapper and make RootChatLayout async

--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -2,23 +2,20 @@ import React from "react";
 import { Header } from "@/components/header";
 import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
-import { ProtectedLayout } from "@/components/layout/ProtectedLayout";
 
-const RootChatLayout = ({ children }: { children: React.ReactNode }) => {
+const RootChatLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
-    <ProtectedLayout>
-      <div className="min-h-screen bg-background relative flex">
-        <SidebarProvider>
-          <AppSidebar />
-          <SidebarInset>
-            <Header />
-            <div className="relative h-[calc(100vh_-64px)] overflow-hidden">
-              {children}
-            </div>
-          </SidebarInset>
-        </SidebarProvider>
-      </div>
-    </ProtectedLayout>
+    <div className="min-h-screen bg-background relative flex">
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <Header />
+          <div className="relative h-[calc(100vh_-64px)] overflow-hidden">
+            {children}
+          </div>
+        </SidebarInset>
+      </SidebarProvider>
+    </div>
   );
 };
 
