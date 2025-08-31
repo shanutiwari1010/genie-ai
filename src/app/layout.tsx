72df52d4879de9a26257050e8b8851392b798398
Shanuti Tiwari <shanutiwari.work@gmail.com>
2025-08-31 18:00:34 +0530

    feat: added cleark provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,7 @@
 import type React from "react";
 import type { Metadata } from "next";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -36,19 +30,6 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-            <SignedIn>
-              <UserButton afterSignOutUrl="/" />
-            </SignedIn>
-            <SignedOut>
-              <div className="flex justify-end gap-2 p-4">
-                <SignInButton mode="modal">
-                  <button className="btn btn-primary">Sign in</button>
-                </SignInButton>
-                <SignUpButton mode="modal">
-                  <button className="btn btn-primary">Sign up</button>
-                </SignUpButton>
-              </div>
-            </SignedOut>
             {children}
             <Toaster />
           </ThemeProvider>
